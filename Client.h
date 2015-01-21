eee6116d4ccd04c4a90b26f240e0a3a2633f5453
dnorth <dnorth2@hotmail.com>
2015-01-20 21:41:54 -0700

    CS 360 Lab 1 Web Client

---
+++ Client.h
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SOCKET_ERROR        -1
+#define BUFFER_SIZE         100
+#define HOST_NAME_SIZE      255
