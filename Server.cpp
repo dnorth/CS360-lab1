f862c0b1f1f95708df2665b078c398c853c5ab27
Danny North <dnorth2@hotmail.com>
2015-01-29 13:43:05 -0700

    Server can now read files. There is still an error because apparently it's not kosher to have strings and switch statements....

--- Server.cpp
+++ Server.cpp
@@ -18,11 +18,35 @@ using namespace std;
 
 string get_file(char* file_path)
 {
-	ifstream ifs(file_path);
+	//Set the mode to be binary
+	ifstream ifs(file_path, ios::in | ios::binary);
 	string content;
 	content.assign( (istreambuf_iterator<char>(ifs) ),
                        (istreambuf_iterator<char>()    ) );
     return content;
+}
+
+string get_contentType(string filename)
+{
+	string file_end = filename.substr( filename.size()-4,4);
+	string filetype= " ";
+	switch(file_end)
+	{
+		case "html":
+			filetype="text/html";
+			break;
+		case ".txt":
+			filetype="text/plain";
+			break;
+		case ".jpg":
+			filetype="image/jpg";
+			break;
+		case ".gif":
+			filetype="image/gif";
+			break;
+	}
+	
+	return filetype;
 }
 
 int main(int argc, char* argv[])
@@ -101,59 +125,71 @@ int main(int argc, char* argv[])
 		
        	if(hSocket < 0)
        	{
-	  printf("ERROR: Could not accept connection");
-	  return 0;
+			printf("ERROR: Could not accept connection");
+			return 0;
        	}
        	else
        	{
-	  //Add new task
-	  printf("\nGot a connection. Reading from the client.\n ");
+			//Add new task
+			printf("\nGot a connection. Reading from the client.\n ");
 			
-	  read(hSocket,pBuffer,sizeof(pBuffer));
-	  char * request_type = strtok(pBuffer, " ");
-	  char * request_url = strtok(pBuffer, " ");
-	  char * request_host = strtok(pBuffer, " ");
-	  printf("Here is what I got: \n\n%s", pBuffer);
+			read(hSocket,pBuffer,sizeof(pBuffer));
+	  
+			printf("Here is what I got: \n\n%s", pBuffer);
+	 
+			char * request_type = strtok(pBuffer, " ");
+			char * request_filepath = strtok(NULL, " ");
 
-	  printf("\n\nType: %s URL: %s Host: %s\n\n", request_type, request_url, request_host);
+			printf("\n\nType: %s URL: %s", request_type, request_filepath);
 			
-	  printf("Writing back.");
+			printf("Writing back.");
 			
-	  //I need to take in the URL from the client (parse the header) and find that file on my server
-	  //Once I find the file on the server I need to retrieve it and send it back with a 200
-			
-	  string contentType="text/html";
-	  char* filepath= "test1.html";
-	  string content= get_file(filepath);
-			
-	  ostringstream stream;
-			
-	  stream << "HTTP/1.1 200 OK\r\nMIME-Version:1.0\r\nContent-Type:" << contentType << "\r\nContent-Length:" << content.length() << "\r\n\r\n" << content; 
-			
-	  string response = stream.str();
-	  char* rPointer = new char[response.length() + 1];
-	  strcpy(rPointer, response.c_str());
-	  printf("Here is the response i'm sending: \n%s\n\n", rPointer);
+		  //I need to take in the URL from the client (parse the header) and find that file on my server
+		  //Once I find the file on the server I need to retrieve it and send it back with a 200
+				
+		  char* filepath= "test4.jpg";
+		  string content= get_file(request_filepath);
+		  string contentType = get_contentType(request_filepath);
+				
+		  if(contentType == " ")
+		  {
+			printf("\nInvalid Content Type.\n");
+			memset(pBuffer, 0, sizeof(pBuffer));
+			if(close(hSocket) == SOCKET_ERROR)
+			{
+			  printf("\nCould not close socket\n");
+			  return 0;
+			}
+			return 0;
+		  }
+		  
+		  ostringstream stream;	
+		  stream << "HTTP/1.1 200 OK\r\nMIME-Version:1.0\r\nContent-Type:" << contentType << "\r\nContent-Length:" << content.length() << "\r\n\r\n" << content; 
+				
+		  string response = stream.str();
+		  char* rPointer = new char[response.length() + 1];
+		  strcpy(rPointer, response.c_str());
+		  printf("Here is the response i'm sending: \n%s\n\n", rPointer);
 
-			
-	  write(hSocket, rPointer, response.length());
-	  printf("\nClosing the socket");
-	  /* close socket */
-	  memset(pBuffer, 0, sizeof(pBuffer));
-	  if(close(hSocket) == SOCKET_ERROR)
-	    {
-	      printf("\nCould not close socket\n");
-	      return 0;
-	    }
-	}
-        //strcpy(pBuffer,MESSAGE);
-		//printf("\nSending \"%s\" to client",pBuffer);
-        /* number returned by read() and write() is the number of bytes
-        ** read or written, with -1 being that an error occured
-        ** write what we received back to the server */
-        //write(hSocket,pBuffer,strlen(pBuffer)+1);
-        /* read from socket into buffer */
-       // memset(pBuffer,0,sizeof(pBuffer));
+				
+		  write(hSocket, rPointer, response.length());
+		  printf("\nClosing the socket");
+		  /* close socket */
+		  memset(pBuffer, 0, sizeof(pBuffer));
+		  if(close(hSocket) == SOCKET_ERROR)
+			{
+			  printf("\nCould not close socket\n");
+			  return 0;
+			}
+		}
+			//strcpy(pBuffer,MESSAGE);
+			//printf("\nSending \"%s\" to client",pBuffer);
+			/* number returned by read() and write() is the number of bytes
+			** read or written, with -1 being that an error occured
+			** write what we received back to the server */
+			//write(hSocket,pBuffer,strlen(pBuffer)+1);
+			/* read from socket into buffer */
+		   // memset(pBuffer,0,sizeof(pBuffer));
 
     }
 }
